42b0e567a36fdb8fb873ae17bc4c061bab1571ca
fredrikfarre <fredrikfarre@gmail.com>
2020-06-02 09:53:36 +0200

    Uppdate DbContext

---
+++ SchoolApp/Data/StudentDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApp.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options){}
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\mssqllocaldb; Initial Catalog = SchoolApp");
+            
+        }
+    }
+}
--- SchoolApp/Models/Course.cs
+++ SchoolApp/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace SchoolApp.Models
     public class Course
     {      
         public int CourseID { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public virtual ICollection<Course> Courses { get; set; }
     }
 }
